3a01d3416e7b356c8144e76a292440ed39f6dd0b
Dhruvik Mevada <dhruvik333@gmail.com>
2021-07-10 21:15:44 +0530

    [IMP] More variable name fixes

--- includes/manager.py
+++ includes/manager.py
@@ -1,34 +1,34 @@
 import json
 from os import path
-from . import check
+from . import utils
 
-OTHER_PASSWORD = check.OTHER_PASSWORDS
+OTHER_PASSWORD = utils.OTHER_PASSWORDS
 d_main = {1: "instagram", 2: "facebook", 3: "twitter", 4: "gmail", 5: "outlook"}
 
 
 class ManagePassword:
     def __init__(self, home_path):
         self.__homePath = home_path
-        self.passwordFile = path.join(self.__homePath, OTHER_PASSWORD)
+        self.password_file = path.join(self.__homePath, OTHER_PASSWORD)
         print(
             "\nAvailable domains are:\n\n1. Instagram\n2. Facebook\n3. Twitter\n4. Gmail\n5. Outlook"
         )
-        self.askDomain = input("Your choice: ")
-        self.add_password = eval(open(self.passwordFile, "r").read())
+        self.ask_domain = input("Your choice: ")
+        self.add_password = eval(open(self.password_file, "r").read())
 
     def add_passwords(self):
         ask_username = input("\nUsername: ")
         ask_password = input("Password: ")
-        pass_list = self.add_password.get(d_main.get(int(self.askDomain)))
+        pass_list = self.add_password.get(d_main.get(int(self.ask_domain)))
 
         pass_list.append({"username": ask_username, "password": ask_password})
-        self.add_password[d_main[int(self.askDomain)]] = pass_list
-        with open(self.passwordFile, "w") as pwdFile:
+        self.add_password[d_main[int(self.ask_domain)]] = pass_list
+        with open(self.password_file, "w") as pwdFile:
             pwdFile.write(json.dumps(self.add_password))
 
     def get_passwords(self):
         ask_username = input("\nUsername: ")
-        uname_list = self.add_password.get(d_main.get(int(self.askDomain)))
+        uname_list = self.add_password.get(d_main.get(int(self.ask_domain)))
         # print(unameList)
         for uKey in uname_list:
             # print(uKey)
@@ -38,3 +38,5 @@ class ManagePassword:
             else:
                 print("\nNo username found!")
                 break
+
+    def list_usernames(self):
--- includes/user.py
+++ includes/user.py
@@ -1,6 +1,6 @@
 import json
 import os
-from . import managePasswords, check
+from . import manager, utils
 
 DOMAINS = {
     "instagram": [],
@@ -13,12 +13,12 @@ DOMAINS = {
 
 class User:
     def __init__(self):
-        if check.os_type() == "win32":
-            self.__homePath = check.get_env("USERPROFILE")
+        if utils.os_type() == "win32":
+            self.__homePath = utils.get_env("USERPROFILE")
         else:
-            self.__homePath = check.get_env("HOME")
+            self.__homePath = utils.get_env("HOME")
 
-        if check.is_pwd_set(self.__homePath, check.MASTER_PASSWORD):
+        if utils.is_pwd_set(self.__homePath, utils.MASTER_PASSWORD):
             passwd = input("Hello! Enter you master password: ")
             self.login_user(passwd)
         else:
@@ -30,23 +30,23 @@ class User:
     def login_user(self, passwd):
         # passwd = input("Hello! Enter you master password: ")
         with open(
-            os.path.join(self.__homePath, check.MASTER_PASSWORD), "r"
+            os.path.join(self.__homePath, utils.MASTER_PASSWORD), "r"
         ) as masterPasswdFile:
             new_passwd = json.loads(masterPasswdFile.read()).get("password")
             if new_passwd == passwd:
-                my_passwords = managePasswords.ManagePassword(self.__homePath)
+                my_passwords = manager.ManagePassword(self.__homePath)
                 _main(my_passwords)
 
     def register_user(self, master_password):
         # masterPassword = input(
         #     "Hello! Never saw you here! Please register yourself with a master password.\nPassword: ")
         with open(
-            os.path.join(self.__homePath, check.MASTER_PASSWORD), "w"
+            os.path.join(self.__homePath, utils.MASTER_PASSWORD), "w"
         ) as masterPasswdFile:
             new_passwd = json.dumps({"password": master_password})
             masterPasswdFile.write(new_passwd)
         with open(
-            os.path.join(self.__homePath, check.OTHER_PASSWORDS), "w"
+            os.path.join(self.__homePath, utils.OTHER_PASSWORDS), "w"
         ) as passwdFile:
             default_passwd = json.dumps(DOMAINS)
             passwdFile.write(default_passwd)
@@ -55,11 +55,13 @@ class User:
 
 def _main(my_passwords):
     while True:
-        ask = input("\n0. Quit\n1. Add password\n2. Get Password\nEnter your choice: ")
+        ask = input("\n0. Quit\n1. Add password\n2. Get Password\n3. List Usernames\nEnter your choice: ")
         if ask == "1":
             my_passwords.add_passwords()
         elif ask == "2":
             my_passwords.get_passwords()
+        elif ask == "3":
+            my_passwords.list_usernames()
         elif ask == "0":
             exit(0)
         else:
